db9078d4e63252cfa4da63cde9541d22ab314a56
stefali1-dev <stefanleustean56@gmail.com>
2023-07-09 14:35:33 +0300

    updated User Entity

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -4,10 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
-
-[ApiController]
-[Route("api/[controller]")]
-public class UsersController
+public class UsersController : BaseApiController
 {
     private readonly DataContext _context;
     public UsersController(DataContext context)
