36dbf9f0a97b5ed29dc8e50dc98bdb3e54d67b35
stefali1-dev <stefanleustean56@gmail.com>
2023-07-12 13:55:23 +0300

    jwt configuration

--- API/Program.cs
+++ API/Program.cs
@@ -1,25 +1,30 @@
+using System.Text;
 using API.Data;
+using API.Extensions;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy(policy => policy.AllowAnyHeader()
-        .AllowAnyMethod().WithOrigins("http://localhost:4200"));
-});
+
+builder.Services.AddApplicationServices(builder.Configuration);
+
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseCors();
+
+app.UseAuthentication();
+app.UseAuthorization();
+ 
 app.MapControllers();
 
 app.Run();
